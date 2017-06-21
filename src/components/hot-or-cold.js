6b4304ff51679433b25e48f16ae9e2e709c35637
System <none>
2017-06-21 13:39:33 +0000

    initial commit

---
+++ src/components/hot-or-cold.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './hot-or-cold.css';
+import GuessForm from './guess-form';
+import Feedback from './feedback';
+import GuessHistory from './guess-history';
+
+export default class HotOrCold extends React.Component {
+    constructor(props) {
+        super(props);      
+        this.state = {
+          answer: this.newAnswer(),
+          guessHistory: []
+        }
+    }
+  
+    handleSubmit(guess) {
+      console.log(guess);
+      const guessHistory = this.state.guessHistory;
+      guessHistory.push(guess);
+      this.setState({  
+        guessHistory
+        });
+    }
+  
+    newAnswer() {
+      return (Math.floor(Math.random() * 100) + 1);
+    }
+  
+    newGame() {
+      this.setState({
+        answer: this.newAnswer(),
+        guessHistory: []
+      });
+    }
+  
+    render() { 
+      console.log('the current state =', this.state)
+      console.log('the guess history =', this.state.guessHistory);
+      
+      
+      return(
+        <div className="content-wrap">
+          <h1>{this.props.title}</h1>
+          <div className="game-box">
+            <p>Guess a Number between 1 and 100</p>
+            <Feedback guessHistory={this.state.guessHistory} answer={this.state.answer} />
+            <GuessForm onSubmit={(guess) => this.handleSubmit(guess)} guessHistory={this.state.guessHistory} 
+              answer={this.state.answer} onClick={() => this.newGame()} />
+            <GuessHistory guessHistory={this.state.guessHistory} />
+          </div>
+        </div>
+      )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HotOrCold from './components/hot-or-cold';
+import './index.css';
+
+ReactDOM.render(
+  <HotOrCold title="HOT or COLD" />,
+  document.getElementById('root')
+);
